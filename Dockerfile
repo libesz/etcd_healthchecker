fe38d342fbda55e268d16303dd5683fe36d5a0a8
Gergo Huszty <gergo.huszty@nokia.com>
2017-12-09 16:39:26 +0000

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM alpine
+
+ADD main.go  /go/
+
+RUN apk add --no-cache go git libc-dev; \
+    cd /go; \
+    go get -v; \
+    go build main.go; \
+    mv main /; \
+    apk del go git libc-dev;
+
+ENTRYPOINT /main
+
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/coreos/etcd/clientv3"
+)
+
+func main() {
+	target := os.Getenv("ETCD_ENDPOINT")
+	if target == "" {
+		log.Fatalf("ETCD_ENDPOINT is not set!")
+	}
+	log.Println("Etcd healthchecker started up! ETCD_ENDPOINT:", target)
+	lastMemberAmount := -1
+	for {
+		cli, err := clientv3.New(clientv3.Config{
+			Endpoints:   []string{target},
+			DialTimeout: 1 * time.Second,
+		})
+		if err != nil {
+			log.Println("Connection failed to", target)
+		} else {
+			defer cli.Close()
+			resp, err := cli.MemberList(context.Background())
+			if err != nil {
+				log.Println("Failed to get memberlist")
+			}
+			if lastMemberAmount != len(resp.Members) {
+				lastMemberAmount = len(resp.Members)
+				memberList := ""
+				for _, element := range resp.Members {
+					memberList += element.Name
+					memberList += ", "
+				}
+				log.Println("Memberlist changed:", memberList)
+			}
+		}
+		time.Sleep(time.Second)
+	}
+}
